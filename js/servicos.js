1a19b4451c5b51fe000ad58d8b1d5038b7dfd001
Evaldo <evaaldormj@gmail.com>
2023-04-10 14:14:13 -0300

    Feat

---
+++ js/servicos.js
@@ -0,0 +1,135 @@
+$(function() {
+    $(".br").click(function() {
+        // cabeçalho
+            // $('.home-cabecalho').children().eq(0).text("HOME")
+            // $('.home-cabecalho').children().eq(1).text("SERVICIOS")
+            // $('.home-cabecalho').children().eq(2).text("SOBRE")
+            // $('.home-cabecalho').children().eq(3).text("CONTACTO")
+            // $('.home-cabecalho-mobile').children().eq(0).text("HOME")
+            // $('.home-cabecalho-mobile').children().eq(1).text("SERVICIOS")
+            // $('.home-cabecalho-mobile').children().eq(2).text("SOBRE")
+            // $('.home-cabecalho-mobile').children().eq(3).text("CONTACTO")
+        $('.language-selected').text("pt-BR")
+        // $('.language-mobile-selected').text("es-ES")
+
+        // banner
+        $('.servicos-banner__titulo').text("Conheça nossos serviços")
+        $('.servicos-banner__subtitulo').text("Conheça todos os nossos serviços aqui em Tatajuba e encontre a plenitude em nossa terra!")
+
+        // mostruário
+        $('.servicos__titulo').text("mostruário")
+        $('#caixa1 a').text("kitesurf")
+        $('#caixa2 a').text("barracas de praia")
+        $('#caixa3 a').text("chales")
+
+        // serviços
+        $('#kitesurf-link').text("KITESURF")
+        $('#kitesurf-texto').text("I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.")
+        $('#barraca-link').text("BARRACAS DE PRAIA")
+        $('#barraca-texto').text("I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.")
+        $('#chale-link').text("CHALÉS")
+        $('#chale-texto').text("I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.")
+    })
+})
+
+$(function() {
+    $(".en").click(function() {
+        // cabeçalho
+            // $('.home-cabecalho').children().eq(0).text("HOME")
+            // $('.home-cabecalho').children().eq(1).text("SERVICIOS")
+            // $('.home-cabecalho').children().eq(2).text("SOBRE")
+            // $('.home-cabecalho').children().eq(3).text("CONTACTO")
+            // $('.home-cabecalho-mobile').children().eq(0).text("HOME")
+            // $('.home-cabecalho-mobile').children().eq(1).text("SERVICIOS")
+            // $('.home-cabecalho-mobile').children().eq(2).text("SOBRE")
+            // $('.home-cabecalho-mobile').children().eq(3).text("CONTACTO")
+        $('.language-selected').text("en-US")
+        // $('.language-mobile-selected').text("es-ES")
+
+        // banner
+        $('.servicos-banner__titulo').text("Conheça nossos serviços")
+        $('.servicos-banner__subtitulo').text("Conheça todos os nossos serviços aqui em Tatajuba e encontre a plenitude em nossa terra!")
+
+        // mostruário
+        $('.servicos__titulo').text("mostruário")
+        $('#caixa1 a').text("kitesurf")
+        $('#caixa2 a').text("barracas de praia")
+        $('#caixa3 a').text("chales")
+
+        // serviços
+        $('#kitesurf-link').text("KITESURF")
+        $('#kitesurf-texto').text("I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.")
+        $('#barraca-link').text("BARRACAS DE PRAIA")
+        $('#barraca-texto').text("I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.")
+        $('#chale-link').text("CHALÉS")
+        $('#chale-texto').text("I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.")
+    })
+})
+
+$(function() {
+    $(".es").click(function() {
+        // cabeçalho
+            // $('.home-cabecalho').children().eq(0).text("HOME")
+            // $('.home-cabecalho').children().eq(1).text("SERVICIOS")
+            // $('.home-cabecalho').children().eq(2).text("SOBRE")
+            // $('.home-cabecalho').children().eq(3).text("CONTACTO")
+            // $('.home-cabecalho-mobile').children().eq(0).text("HOME")
+            // $('.home-cabecalho-mobile').children().eq(1).text("SERVICIOS")
+            // $('.home-cabecalho-mobile').children().eq(2).text("SOBRE")
+            // $('.home-cabecalho-mobile').children().eq(3).text("CONTACTO")
+        $('.language-selected').text("es-ES")
+        // $('.language-mobile-selected').text("es-ES")
+
+        // banner
+        $('.servicos-banner__titulo').text("Conheça nossos serviços")
+        $('.servicos-banner__subtitulo').text("Conheça todos os nossos serviços aqui em Tatajuba e encontre a plenitude em nossa terra!")
+
+        // mostruário
+        $('.servicos__titulo').text("mostruário")
+        $('#caixa1 a').text("kitesurf")
+        $('#caixa2 a').text("barracas de praia")
+        $('#caixa3 a').text("chales")
+
+        // serviços
+        $('#kitesurf-link').text("KITESURF")
+        $('#kitesurf-texto').text("I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.")
+        $('#barraca-link').text("BARRACAS DE PRAIA")
+        $('#barraca-texto').text("I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.")
+        $('#chale-link').text("CHALÉS")
+        $('#chale-texto').text("I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.")
+    })
+})
+
+$(function() {
+    $(".fr").click(function() {
+        // cabeçalho
+            // $('.home-cabecalho').children().eq(0).text("HOME")
+            // $('.home-cabecalho').children().eq(1).text("SERVICIOS")
+            // $('.home-cabecalho').children().eq(2).text("SOBRE")
+            // $('.home-cabecalho').children().eq(3).text("CONTACTO")
+            // $('.home-cabecalho-mobile').children().eq(0).text("HOME")
+            // $('.home-cabecalho-mobile').children().eq(1).text("SERVICIOS")
+            // $('.home-cabecalho-mobile').children().eq(2).text("SOBRE")
+            // $('.home-cabecalho-mobile').children().eq(3).text("CONTACTO")
+        $('.language-selected').text("fr-FR")
+        // $('.language-mobile-selected').text("es-ES")
+
+        // banner
+        $('.servicos-banner__titulo').text("Conheça nossos serviços")
+        $('.servicos-banner__subtitulo').text("Conheça todos os nossos serviços aqui em Tatajuba e encontre a plenitude em nossa terra!")
+
+        // mostruário
+        $('.servicos__titulo').text("mostruário")
+        $('#caixa1 a').text("kitesurf")
+        $('#caixa2 a').text("barracas de praia")
+        $('#caixa3 a').text("chales")
+
+        // serviços
+        $('#kitesurf-link').text("KITESURF")
+        $('#kitesurf-texto').text("I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.")
+        $('#barraca-link').text("BARRACAS DE PRAIA")
+        $('#barraca-texto').text("I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.")
+        $('#chale-link').text("CHALÉS")
+        $('#chale-texto').text("I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.")
+    })
+})
